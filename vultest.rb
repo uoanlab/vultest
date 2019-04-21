c1d0a83a2dbca19953506ccd2d84ecd5e2cb0cf6
redsloop <redsloop.ko@gmail.com>
2019-04-21 18:58:15 +0900

    [add] コマンドライン上で、テストをできるように修正した
    コマンドラインでオプションを指定して、テストをできるように変更した

--- vultest.rb
+++ vultest.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
+require 'optparse'
 require 'pastel'
 require 'tty-font'
 
@@ -6,6 +7,46 @@ require_relative './utility.rb'
 require_relative './commands/test_command.rb'
 require_relative './commands/vultest_command.rb'
 
+# setting initialize var
+testdir = './test'
+testdir = ENV['TESTDIR'] if ENV.key?('TESTDIR')
+
+attacker = nil
+attacker = ENV['ATTACKER'] if ENV.key?('ATTACKER')
+
+cve = nil
+vulenv_config_path = nil
+attack_config_path = nil
+
+if ARGV.size != 0
+  options = ARGV.getopts('c:', 'a:', 't:', 'd')
+
+  exit! if options['c'].nil?
+  cve = options['c']
+
+  # setting options
+  attacker = options['a'] unless options['a'].nil?
+  testdir = options['t'] unless options['t'].nil?
+
+  # execute vulnerable test
+  begin
+    vulenv_config_path, attack_config_path = VultestCommand.test(cve, testdir)
+  rescue
+    Utility.print_message('error', "Cannot test #{cve}")
+    exit!
+  end
+
+  begin
+    TestCommand.exploit(attacker, testdir, vulenv_config_path, attack_config_path)
+    TestCommand.report(cve, vulenv_config_path, attack_config_path)
+  rescue
+    retry
+  end
+
+  TestCommand.destroy(testdir) if options['d']
+
+  exit!
+end
 
 # vultest title
 font = TTY::Font.new(:"3d")
@@ -14,15 +55,6 @@ puts pastel.red(font.write("VULTEST"))
 
 # pormpt and option initialize
 prompt = 'vultest'
-cve = nil
-vulenv_config_path = nil
-attack_config_path = nil
-
-testdir = './test'
-testdir = ENV['TESTDIR'] if ENV.key?('TESTDIR')
-
-attacker = nil
-attacker = ENV['ATTACKER'] if ENV.key?('ATTACKER')
 
 # execute prompt
 loop do
@@ -32,7 +64,14 @@ loop do
   case input_list[0]
   when 'test'
     cve = input_list[1]
-    vulenv_config_path, attack_config_path = VultestCommand.test(cve, testdir)
+
+    begin
+      vulenv_config_path, attack_config_path = VultestCommand.test(cve, testdir)
+    rescue
+      Utility.print_message('error', "Cannot test #{cve}")
+      TestCommand.destroy(testdir)
+    end
+
     unless vulenv_config_path.nil? && attack_config_path.nil?
       prompt = cve
     end
@@ -41,7 +80,12 @@ loop do
     break if VultestCommand.exit == 'success'
 
   when 'exploit'
-    TestCommand.exploit(attacker, testdir, vulenv_config_path, attack_config_path)
+    begin
+      TestCommand.exploit(attacker, testdir, vulenv_config_path, attack_config_path)
+    rescue
+      Utility.print_message('error', "Cannot exploit")
+      TestCommand.destroy(testdir)
+    end
 
   when 'set'
     if input_list.length != 3
@@ -52,7 +96,12 @@ loop do
     testdir = TestCommand.set(input_list[1], input_list[2]) if input_list[1] == 'TESTDIR'
 
   when 'report'
-    TestCommand.report(cve, vulenv_config_path, attack_config_path)
+    begin
+      TestCommand.report(cve, vulenv_config_path, attack_config_path)
+    rescue
+      Utility.print_message('error', 'Cannot output vulnerable report')
+      TestCommand.destroy(testdir)
+    end
 
   when 'destroy'
     TestCommand.destroy(testdir)
