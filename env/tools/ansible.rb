e8c86e276dd835377591db844a539645f00a1d15
redsloop <redsloop.ko@gmail.com>
2019-05-09 20:01:09 +0900

    [fix] ライセンスの修正

--- env/tools/ansible.rb
+++ env/tools/ansible.rb
@@ -12,40 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-Copyright © 2008 Jamis Buck
-Relased under the MIT license
-https://github.com/net-ssh/net-ssh/blob/master/LICENSE.txt
-
-Copyright (c) Marcin Kulik
-Relased under the MIT license
-https://github.com/sickill/rainbow/blob/master/LICENSE
-
-Copyright (c) 2016 Piotr Murach
-Relased under the MIT license
-https://github.com/piotrmurach/tty-command/blob/master/LICENSE.txt
-
-Copyright (c) 2015 Piotr Murach
-Relased under the MIT license
-https://github.com/piotrmurach/tty-prompt/blob/master/LICENSE.txt
-
-Copyright (c) 2014 Piotr Murach
-Relased under the MIT license
-https://github.com/piotrmurach/tty-spinner/blob/master/LICENSE.txt
-
-Copyright (c) 2015 Piotr Murach
-Relased under the MIT license
-https://github.com/piotrmurach/tty-table/blob/master/LICENSE.txt
-
-Copyright (c) 2014 Piotr Murach
-Relased under the MIT license
-https://github.com/piotrmurach/pastel/blob/master/LICENSE.txt
-
-Copyright (c) 2017 Piotr Murach
-Relased under the MIT license
-https://github.com/piotrmurach/tty-font/blob/master/LICENSE.txt
-
-This software includes the work that is distributed in the Apache License 2.0
 =end
 
 require_relative '../../utility'
