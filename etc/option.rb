ccbb73097ee859e56c857c4de79121b1bf7ed6cc
redsloop <redsloop.ko@gmail.com>
2019-09-18 15:29:17 +0900

    Refactoring

--- etc/option.rb
+++ etc/option.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require_relative './process/vultest'
+require_relative '../proc/vultest'
 
 module VultestOptionExecute
   class << self
