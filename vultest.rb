74a2d85e9746b8a320a6e0d2f9cbb95677edf1c8
Kohei Akasaka <redsloop.ko@gmail.com>
2020-01-24 02:11:38 +0900

    Refactoring

--- vultest.rb
+++ vultest.rb
@@ -13,37 +13,94 @@
 # limitations under the License.
 
 require 'bundler/setup'
+require 'pastel'
+require 'tty-font'
 require 'optparse'
 
-require './app/console/console'
-require './app/option'
+require './lib/command'
+require './lib/ui'
 
-unless ARGV.size.zero?
-  options = ARGV.getopts('h', 'cve:', 'test:yes', 'attack_user:', 'attack_passwd:', 'attack_host:', 'dir:', 'destroy:')
-  VultestOptionExecute.execute_vultest(options)
-  exit!
-end
+puts Pastel.new.red(TTY::Font.new(:"3d").write('VULTEST'))
+console = {}
+console[:prompt] = TTY::Prompt.new(active_color: :cyan, help_color: :bright_white, track_history: true)
+console[:name] = 'vultest'
 
-console = Console.new
-console.initialize_prompt
-console.initialize_vultest_process
+setting = {}
+setting[:test_dir] = ENV.key?('TESTDIR') ? ENV['TESTDIR'] : './test_dir'
+setting[:attack_host] = ENV.key?('ATTACKHOST') ? ENV['ATTACKHOST'] : nil
+setting[:attack_user] = ENV.key?('ATTACKERUSER') ? ENV['ATTACKERUSER'] : 'root'
+setting [:attack_passwd] = ENV.key?('ATTACKPASSWD') ? ENV['ATTACKPASSWD'] : nil
+
+vultest_case = nil
+vulenv = nil
+attack_env = nil
 
 loop do
-  command = console.prompt.ask("#{console.prompt_name} >")
-  command.nil? ? next : command = command.split(' ')
+  cmd = console[:prompt].ask("#{console[:name]} >")
+  cmd.nil? ? next : cmd = cmd.split(' ')
 
-  case command[0]
-  when /test/i then console.execute_test_command(cve: command[1])
-  when /exit/i then break
-  when /exploit/i then console.execute_exploit_command
-  when /set/i then console.execute_option_command(option_type: command[1], option_value: command[2])
-  when /report/i then console.execute_report_command
-  when /destroy/i then console.execute_destroy_command
-  when /back/i
-    if console.execute_back_command
-      console.prompt_name = 'vultest'
-      console.initialize_vultest_process
+  case cmd[0]
+  when /test/i
+    console[:name], vultest_case, vulenv = Command.test(
+      cve: cmd[1],
+      vultest_case: vultest_case,
+      vulenv_dir: setting[:test_dir]
+    )
+
+  when /destroy/i
+    vulenv = Command.destroy(prompt: console[:prompt], vulenv: vulenv)
+
+  when /exploit/i
+    if vultest_case.vulenv_config['attack_vector'] == 'local'
+      setting[:attack_host] = '192.168.177.177'
+      VultestUI.execute('Change value of ATTACKHOST')
+      console[:prompt].ok('ATTACKHOST => 192.168.177.177')
+    elsif vultest_case.vulenv_config['attack_vector'] == 'remote' && setting[:attack_host].nil?
+      VultestUI.error('Cannot find the attack host')
+      VultestUI.warring('Execute : SET ATTACKHOST attack_host_ip_address')
+      next
     end
-  else console.prompt.error("vultest: command not found: #{command[0]}")
+
+    attack_env = Command.exploit(
+      vultest_case: vultest_case,
+      vulenv: vulenv,
+      attack_host: setting[:attack_host],
+      attack_user: setting[:attack_user],
+      attack_passwd: setting[:attack_passwd]
+    )
+
+  when /report/i
+    Command.report(
+      vultest_case: vultest_case,
+      vulenv: vulenv,
+      attack_env: attack_env,
+      report_dir: setting[:test_dir]
+    )
+
+    next if vulenv.error[:flag] || attack_env.nil? || attack_env.error[:flag]
+
+    attack_env.rob_shell
+
+  when /set/i
+    Command.set(
+      prompt: console[:prompt],
+      setting: setting,
+      vulenv: vulenv,
+      attack_env: attack_env,
+      type: cmd[1],
+      value: cmd[2]
+    )
+
+  when /back/i
+    console[:name], vultest_case, vulenv, attack_env = Command.back(
+      prompt: console[:prompt],
+      name: console[:name],
+      vultest_case: vultest_case,
+      vulenv: vulenv,
+      attack_env: attack_env
+    )
+
+  when /exit/i then break
+  else console[:prompt].error("vultest: command not found: #{cmd[0]}")
   end
 end
